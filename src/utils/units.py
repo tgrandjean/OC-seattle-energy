42698224cdef0b8213f97509dc30730f9405c46c
Thibault Grandjean <thibault.grandjean@protonmail.ch>
2020-01-16 19:51:18 +0100

    :bug: Forgot to pass the "cls" object in class method.

--- src/utils/units.py
+++ src/utils/units.py
@@ -4,10 +4,11 @@
 class Converter(object):
 
     @classmethod
-    def kbtu_to_kwh(energy):
+    def kbtu_to_kwh(cls, energy):
         """Conversion from kilo british thermal unit to kilo watt hour.
 
         As a reminder : 3412 kwh = 1 btu
+        more precisly : 3412.1416299 kwh = 1 btu
         So we multiply the energy value by thousand and then we divide the
         all by 3412 to obtain the value of energy in kwh.
 
@@ -20,13 +21,14 @@ class Converter(object):
             >>>ktbu_to_kwh(3.412)
             1.0
         """
-        return (energy * 1e3) / 3412
+        return (energy * 1e3) / 3412.1416299
 
     @classmethod
-    def sf_to_square_m(area):
+    def sf_to_square_m(cls, area):
         """Conversion from square foot to square meters.
 
         As a reminder : 10.764 sf = 1 m2
+        Conversion rate : 10.76391041671
         So we divide the area value in square foot by 10.764 to obtain the
         area value in square meters.
 
@@ -35,10 +37,10 @@ class Converter(object):
         :returns:
             area (float) : the value of area converted in m2
         """
-        return area / 10.764
+        return area / 10.76391041671
 
     @classmethod
-    def kbtu_per_sf_to_kwh_per_sm(value):
+    def kbtu_per_sf_to_kwh_per_sm(cls, value):
         """Conversion from square foot to square meters.
 
         :args:
@@ -46,4 +48,4 @@ class Converter(object):
         :returns:
             area (float) : the value of energy density converted in kwh/m2
         """
-        return Converter.kbtu_to_kwh(value) / Converter.sf_to_square_m(1)
+        return cls.kbtu_to_kwh(value) / cls.sf_to_square_m(1)
