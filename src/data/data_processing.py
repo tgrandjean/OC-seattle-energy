6d2b4266ec400805a473d748e96a3ace9d8319c0
Thibault Grandjean <thibault.grandjean@protonmail.ch>
2020-02-11 10:08:52 +0100

    :construction_worker: data processing pipeline

--- src/data/data_processing.py
+++ src/data/data_processing.py
@@ -32,6 +32,31 @@ class AbstractPipeline(ABC):
     def data(self, data):
         if type(data) != pd.DataFrame:
             raise ValueError("You should pass a pandas dataframe obj.")
+        self._data = data
+
+    @property
+    def target(self):
+        """Get or set target variable **y** of the model.
+
+        target (str or list): string or list of target variables.
+
+        :note:
+            must be present in self.data.columns
+        """
+        return self._target
+
+    @target.setter
+    def target(self, target):
+        if type(target) != str and type(target) != list:
+            raise ValueError('Incorrect type for target')
+        elif type(target) == str:
+            if target not in self.data.columns:
+                raise IndexError("Invalid target name %s" % target)
+        elif type(target) == list:
+            for elt in target:
+                if elt not in self.data.columns:
+                    raise IndexError('Invalid target name %s' % elt)
+        self._target = target
 
     def process(self):
         """process the pipeline.
@@ -40,3 +65,16 @@ class AbstractPipeline(ABC):
         Execute this method to run the pipeline.
         """
         raise NotImplementedError("Override this method.")
+
+
+class ProcessingPipeline(AbstractPipeline):
+    """Data processing pipeline.
+
+    :usage:
+        >> data = pd.DataFrame({'x': np.arange(10),
+        >>                      'y': np.arange(10)})
+        >> pipeline = ProcessingPipeline(data)
+    """
+
+    def process(self):
+        pass
