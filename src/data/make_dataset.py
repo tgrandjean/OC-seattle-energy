dd383264905540d568796a90d1bcd8a753adb85b
thibault <thibault.grandjean@icloud.com>
2020-02-13 15:04:04 +0100

    :bug: init_data_dir use path lib and run before main func

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+import logging
 import os
+from pathlib import Path
+
 import click
 import kaggle
-import logging
-from pathlib import Path
 
 DATASET_NAME = 'city-of-seattle/sea-building-energy-benchmarking'
 
@@ -18,32 +19,31 @@ def download_dataset(name, output_filepath, **kwargs):
                                       unzip=True)
 
 
-def init_data_dir(filepath):
+def init_data_dir(project_dir):
     """Create empty dir if they not exists"""
     logger = logging.getLogger(__name__)
     logger.info("Init data directories")
-    root_dir = '/'.join(filepath.split('/')[:-1])
+    filepath = Path(project_dir).resolve()
     data_dirs = ['external',
                  'interim',
                  'processed',
                  'raw']
-    if not os.path.exists(root_dir):
-        os.mkdir(root_dir)
-        for dir in data_dirs:
-            os.mkdir(os.path.join(root_dir, dir))
+    data_path = filepath.joinpath('data')
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
+        for directory in data_dirs:
+            os.mkdir(data_path.joinpath(directory))
     else:
         logger.info('already exists')
-
 
 def make_data(input_filepath):
     logger = logging.getLogger(__name__)
     logger.info('Making the dataset.')
-    init_data_dir(input_filepath)
     download_dataset(DATASET_NAME, output_filepath=input_filepath)
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
+@click.argument('input_filepath', type=click.Path())
 @click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
@@ -56,7 +56,7 @@ def main(input_filepath, output_filepath):
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logging.basicConfig(level=logging.DEBUG, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
@@ -64,5 +64,5 @@ if __name__ == '__main__':
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     # load_dotenv(find_dotenv())
-
+    init_data_dir(project_dir)
     main()
