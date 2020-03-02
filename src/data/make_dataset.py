8973d261462da97d68d18fc39d3300c9fb708699
Thibault Grandjean <thibault.grandjean@protonmail.ch>
2020-03-02 18:44:07 +0100

    :sparkles: get more data :)

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,11 +2,23 @@
 import logging
 import os
 from pathlib import Path
+from string import Template
 
 import click
 import kaggle
+import requests
+from tqdm import tqdm
+
 
 DATASET_NAME = 'city-of-seattle/sea-building-energy-benchmarking'
+BASE_URL = 'https://data.seattle.gov/api/views/$id/rows.csv?accessType=DOWNLOAD'
+
+SET_IDs = {
+    # 2015 : 'h7rm-fz6m', # from Kaggle
+    # 2016 : '2bpz-gwpy',
+    2017: 'qxjw-iwsh',
+    2018: '7rac-kyay'
+    }
 
 
 def download_dataset(name, output_filepath, **kwargs):
@@ -18,6 +30,26 @@ def download_dataset(name, output_filepath, **kwargs):
                                       path=output_filepath,
                                       unzip=True)
 
+def download_dataset_seattle(url, output_filepath, **kwargs):
+    """Download the dataset."""
+    logger = logging.getLogger(__name__)
+    logger.info("Start download, this can take a while...")
+    user_agent = kwargs.get('user_agent', 'Mozilla/5.0')
+    encoding = kwargs.get('encoding', None)
+    headers = {'User-agent' : user_agent,
+               'Accept-Encoding': encoding
+               }
+    logger.debug(headers)
+    response = requests.get(url, stream=True, headers=headers)
+    logger.debug("response's headers : %s", response.headers)
+    content_disp = response.headers.get('Content-disposition')
+    filename = content_disp.split('=')[1]
+    logger.debug("filename %s", filename)
+    with open(os.path.join(output_filepath, filename), 'wb') as file:
+        chunk_size = 1024
+        for data in tqdm(response.iter_content(chunk_size=chunk_size),
+                  desc='Download data'):
+                  file.write(data)
 
 def init_data_dir(project_dir):
     """Create empty dir if they not exists"""
@@ -41,6 +73,10 @@ def make_data(input_filepath):
     logger = logging.getLogger(__name__)
     logger.info('Making the dataset.')
     download_dataset(DATASET_NAME, output_filepath=input_filepath)
+    for name, id in SET_IDs.items():
+        download_dataset_seattle(Template(BASE_URL).substitute(id=id),
+                                 os.path.join(input_filepath),
+                                 encoding='gzip')
 
 
 @click.command()
